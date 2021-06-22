b16c1e32144b08070c3f1b7bfcd508fc8753319e
JW558870 <81600779+JW558870@users.noreply.github.com>
2021-06-23 00:03:11 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(320, 320);
+  createCanvas(300, 300);
   // Create the video
   var constraints = {
 
@@ -51,7 +51,7 @@ function setup() {
   };
   
   video = createCapture(constraints);
-  video.size(320, 320);
+  video.size(300, 300);
   video.hide();
 
   flippedVideo = ml5.flipImage(video)
